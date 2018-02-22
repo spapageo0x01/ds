55e80cce9f4354f6a25274af5676884c63c7e171
Spyros Papageorgiou <spapageo0x01@gmail.com>
2018-02-22 20:05:32 +0200

    Code-complete doubly linked lists

--- 1_doubly_linked_list/doubly_linked_list.c
+++ 1_doubly_linked_list/doubly_linked_list.c
@@ -29,7 +29,8 @@ dlist_t * dlist_init(int *error)
 // Time complexity: O(N) worst case
 int dlist_destroy(dlist_t *list, int *error)
 {
-	dnode_t * dest, temp;
+	dnode_t * dest;
+	dnode_t * temp;
 	*error = NO_ERROR;
 
 	if (!list) {
@@ -74,11 +75,9 @@ size_t dlist_length(dlist_t *list, int *error)
 	return list->length;
 }
 
-//------------------------------------------------------------
-
 
 // Return: EXIT_SUCCESS on success, EXIT_FAILURE on failure
-// Description: Allocates a new node and inserts it a the head of the list
+// Description: Allocates a new node and inserts it at the head of the list
 // Time complexity: O(1)
 int dlist_insert_head(dlist_t *list, void *data, int *error)
 {
@@ -110,6 +109,219 @@ int dlist_insert_head(dlist_t *list, void *data, int *error)
 
 	return EXIT_SUCCESS;
 }
+
+
+// Return: EXIT_SUCCESS on success, EXIT_FAILURE on failure
+// Description: Allocates a new node and inserts it at the tail of the list
+// Time complexity: O(1)
+int dlist_insert_tail(dlist_t *list, void *data, int *error)
+{
+	dnode_t *new_node, *temp;
+	*error = NO_ERROR;
+
+	if (list == NULL) {
+		*error = ERROR_NULL;
+		return EXIT_FAILURE;
+	}
+
+	new_node = malloc(sizeof(dnode_t));
+	if (new_node == NULL) {
+		*error = ERROR_ALLOC;
+		return EXIT_FAILURE;
+	}
+
+	new_node->data = data;
+	new_node->next = NULL;
+	new_node->prev = NULL;
+
+	pthread_mutex_lock(&list->lock);
+	if (list->head == NULL) {
+		list->head = new_node;
+	} else {
+		temp = list->head;
+		while (temp->next) {
+			temp = temp->next;
+		}
+		temp->next = new_node;
+		new_node->prev = temp;
+	}
+	++list->length;
+	pthread_mutex_unlock(&list->lock);
+
+	return EXIT_SUCCESS;
+}
+
+
+// Return: EXIT_SUCCESS on success, EXIT_FAILURE on failure
+// Description: Allocates a new node and inserts it sorted within the list (based on the compare 
+// function)
+// Time complexity: O(1)
+int dlist_insert_sorted(dlist_t *list, void *data, int (*compare)(const void *, const void *), int *error)
+{
+	dnode_t *new_node, *temp, *prev;
+	*error = NO_ERROR;
+
+	// Check list provided is not NULL
+	if (list == NULL) {
+		*error = ERROR_NULL;
+		return EXIT_FAILURE;
+	}
+
+	// Allocate new node
+	new_node = malloc(sizeof(dnode_t));
+	if (new_node == NULL) {
+		*error = ERROR_ALLOC;
+		return EXIT_FAILURE;
+	}
+
+	// Initialize new node
+	new_node->data = data;
+	new_node->next = NULL;
+	new_node->prev = NULL;
+
+	// Acquire list lock
+	pthread_mutex_lock(&list->lock);
+	if (list->head == NULL) {
+		// 0. If list head is NULL, this is the only node. Simply add
+		list->head = new_node;
+	} else {
+		// 1. The list is not empty. Traverse it until compare() returns 1. Insert
+		// the new node before the "temp" node.
+		temp = list->head;
+		do {
+			if (compare(data, temp->data)) {
+				new_node->next = temp;
+				new_node->prev = temp->prev;
+				temp->prev = new_node;
+				break;
+			}
+			prev = temp;
+			temp = temp->next;
+		} while (temp);
+		
+
+		// 2. Compare never evaluated as true, therefore simply add node to the tail of the list.
+		if (temp == NULL) {
+			prev->next = new_node;
+			new_node->prev = prev;
+		}
+
+	}
+	++list->length;
+	pthread_mutex_unlock(&list->lock);
+
+	return EXIT_SUCCESS;
+}
+
+
+// Time complexity: O(n)
+int dlist_lookup(dlist_t *list, void *data, int (*isequal)(const void *, const void *), int *error)
+{
+	int found = 0;
+	dnode_t *tmp;
+	*error = NO_ERROR;
+
+	if (!list) {
+		*error = ERROR_NULL;
+		return 0;
+	}
+
+	pthread_mutex_lock(&list->lock);
+	tmp = list->head;
+	while (tmp) {
+		if (isequal(data, tmp->data)) {
+			found = 1;
+			break;
+		}
+		tmp = tmp->next;
+	}
+	pthread_mutex_unlock(&list->lock);
+
+	return found;
+}
+
+
+// Time complexity: O(1)
+void * dlist_remove_head(dlist_t *list, int *error)
+{
+	void * data = NULL;
+	dnode_t *node;
+	*error = NO_ERROR;
+
+	if (!list) {
+		*error = ERROR_NULL;
+		return NULL;
+	}
+
+	if (!list->head) {
+		return NULL;
+	}
+
+	pthread_mutex_lock(&list->lock);
+	node = list->head;
+	list->head = list->head->next;
+	list->head->prev = NULL;
+	--list->length;
+	pthread_mutex_unlock(&list->lock);
+
+	data = node->data;
+	free(node);
+
+	return data;
+}
+
+
+
+// Time complexity: O(n)
+void * dlist_remove_tail(dlist_t *list, int *error)
+{
+	void * data = NULL;
+	dnode_t *node;
+	*error = NO_ERROR;
+
+	if (!list) {
+		*error = ERROR_NULL;
+		return NULL;
+	}
+
+	if (!list->head) {
+		return NULL;
+	}
+
+	pthread_mutex_lock(&list->lock);
+	node = list->head;
+	while (node->next) {
+		node = node->next;
+	}
+	node->prev->next = NULL;
+	--list->length;
+	pthread_mutex_unlock(&list->lock);
+
+	data = node->data;
+	free(node);
+
+	return data;
+}
+
+
+
+/*
+static void __dlist_print(dlist_t *list)
+{
+	dnode_t *tmp;
+	if (list == NULL) {return;}
+	if (list->head == NULL) {return;}
+
+	pthread_mutex_lock(&list->lock);
+	tmp = list->head;
+	while (tmp) {
+		printf("%d ", tmp->key);
+		tmp = tmp->next;
+	}
+	printf("\n");
+	pthread_mutex_unlock(&list->lock);
+}
+*/
 
 /*
 
@@ -176,23 +388,6 @@ int dlist_delete(dlist_t *L, int key) {
 	return EXIT_SUCCESS;
 }
 
-
-
-void dlist_print(dlist_t *L)
-{
-	dnode_t *tmp;
-	if (L == NULL) {return;}
-	if (L->head == NULL) {return;}
-
-	pthread_mutex_lock(&L->lock);
-	tmp = L->head;
-	while(tmp) {
-		printf("%d ", tmp->key);
-		tmp = tmp->next;
-	}
-	printf("\n");
-	pthread_mutex_unlock(&L->lock);
-}
 
 */
 
--- 1_doubly_linked_list/doubly_linked_list.h
+++ 1_doubly_linked_list/doubly_linked_list.h
@@ -20,12 +20,11 @@ int dlist_destroy(dlist_t *list, int *error);	// OK
 int dlist_isempty(dlist_t *list);				// OK
 size_t dlist_length(dlist_t *list, int *error);	// OK
 
+int dlist_insert_head(dlist_t *list, void *data, int *error);	//OK
+int dlist_insert_tail(dlist_t *list, void *data, int *error);	//OK
+int dlist_insert_sorted(dlist_t *list, void *data, int (*compare)(const void *, const void *), int *error);	// OK
 
-int dlist_insert_head(dlist_t *list, void *data, int *error);	//TODO
-int dlist_insert_tail(dlist_t *list, void *data, int *error);	//TODO
-int dlist_insert_sorted(dlist_t *list, void *data, int (*compare)(const void *, const void *), int *error);	// TODO
+int dlist_lookup(dlist_t *list, void *data, int (*isequal)(const void *, const void *), int *error); // OK
 
-int dlist_lookup(dlist_t *list, void *data, int (*isequal)(const void * const void *), int *error);	// TODO
-
-dnode_t * dlist_remove_head(dlist_t *list, int *error);	//TODO
-dnode_t * dlist_remove_tail(dlist_t *list, int *error); // TODO
+void * dlist_remove_head(dlist_t *list, int *error); // OK
+void * dlist_remove_tail(dlist_t *list, int *error); // OK
