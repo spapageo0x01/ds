02622ba9e7bd2e0ec77bdfa86bac59729d6f0391
Spyros Papageorgiou <spapageo0x01@gmail.com>
2018-02-26 10:55:05 +0200

    Hash table

---
+++ 4_hash_table/hash_table.h
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <pthread.h>
+#include "../0_linked_list/linked_list.h"
+
+#define BUCKETS 101
+
+typedef struct __string_t {
+	char *str;
+	size_t length;
+} string_t;
+
+typedef struct __hash_t {
+	list_t lists[BUCKETS];
+} hash_t;
+
+hash_t * hash_init(int *error);
+void hash_destroy(hash_t *hash_table, int *error);
+
+int hash_lookup(hash_t *hash_table, string_t *data, int *error);
+
+int hash_insert(hash_t *hash_table, string_t *data, int *error);
+int hash_delete(hash_t *hash_table, string_t *data, int *error);
